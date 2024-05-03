1c607a46d37a6589342112193fa37a023c3aa4fb
IAmProgrammist <vladislav.pakhomov@bk.ru>
2024-05-03 15:08:16 +0300

    Custom task preparation for linearization

---
+++ src/lab2/main_custom.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <math.h>
+
+#define dt ((20.0) / (30000.0))
+#define PI 3.141592654
+#define g 9.81
+#define k1 5000.0
+#define k2 7000.0
+#define I 1.0
+#define R 1.0
+#define r 0.2
+#define m 10.0
+#define l0 0
+#define es 0.000001
+
+double degreeToRadians(double angle) {
+    return angle * (PI / 180.0);
+}
+
+double getC(double angle) {
+    return std::sqrt(r*r + r*r - 2*r*r*std::cos(degreeToRadians(angle)));
+}
+
+double getDeltaL(double angle) {
+    return std::sqrt(getC(angle) * getC(angle) + l0*l0 - 2*l0*getC(angle)*std::cos(degreeToRadians(180 - angle / 2))) - l0;
+}
+
+double getAngleAcc(double angle) {
+    return -(
+    // M1
+    k1 * getDeltaL(angle) * r +
+        
+    // M2
+    - (m*g) * R) / I;
+}
+
+double getAngleVel(double w) {
+    return w;
+}
+
+double getYAcc(double y, double angle) {
+    return (m * g - k2 * (y - angle * PI * R / 180.0)) / m;
+}
+
+double getY(double v) {
+    return v;
+}
+
+int main() {
+    double angle = 0.0;
+    double aV = 0.0;
+
+    double y = 0.0;
+    double yV = 0.0;
+
+    for (double t = 0.0; t < 40; t += dt) {
+        // Recalc aV
+        double aVk1 = getAngleAcc(angle);
+        double aVk2 = getAngleAcc(angle + (dt/2) * aVk1);
+        double aVk3 = getAngleAcc(angle + (dt/2) * aVk2);
+        double aVk4 = getAngleAcc(angle + dt * aVk3);
+        aV += (dt/6) * (aVk1 + 2 * aVk2 + 2 * aVk3 + aVk4);
+        
+        // Recalc angle
+        double ak1 = getAngleVel(aV);
+        double ak2 = getAngleVel(aV + (dt/2) * ak1);
+        double ak3 = getAngleVel(aV + (dt/2) * ak2);
+        double ak4 = getAngleVel(aV + dt * ak3);
+        angle += (dt/6) * (ak1 + 2 * ak2 + 2 * ak3 + ak4);
+        
+        // Recalc yV
+        double yVk1 = getYAcc(y, angle);
+        double yVk2 = getYAcc(y + (dt/2) * yVk1, angle + dt/2);
+        double yVk3 = getYAcc(y + (dt/2) * yVk2, angle + dt/2);
+        double yVk4 = getYAcc(y + dt * yVk3, angle + dt);
+        yV += (dt/6) * (yVk1 + 2 * yVk2 + 2 * yVk3 + yVk4);
+        
+        // Recalc y
+        double yk1 = getY(yV);
+        double yk2 = getY(yV + (dt/2) * yk1);
+        double yk3 = getY(yV + (dt/2) * yk2);
+        double yk4 = getY(yV + dt * yk3);
+        y += (dt/6) * (yk1 + 2 * yk2 + 2 * yk3 + yk4);
+
+        std::cout << t << " " << angle << " " << y << std::endl;
+    }
+}
